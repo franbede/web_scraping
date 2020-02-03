0706da50bcdbe6b109f8e26ac0a4594de1d21468
Francisco Benavente <francisco.benavente@emite-ingenieria.es>
2020-02-03 14:31:19 +0100

    Windows 10 version included

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # web_scraping
 Examples of web scraping with Python
 
-Most of the intial code has been extracted from:
+Most of the intial code has been extracted from:  
 https://thinkdiff.net/python/how-to-run-javascript-in-python-web-scraping-testing/
 
-## Notes on Google Chrome driver:  
+## Selenium version
+pip install selenium  
+Version used: 3.141.0 (latest)
+
+## Notes on Google Chrome driver:
 Version -> ChromeDriver 79.0.3945.36  
-OS -> macOS
+OS -> macOS (chromedriver), Windows (chromedriver.exe)
+
+## How to create stand-alone installer for Python scripts
+Use PyInstaller: https://www.pyinstaller.org/  
+Steps:
+1) pip install pyinstaller
+2) pyinstaller program.py
---
+++ web_scraping_Win10.py
@@ -0,0 +1,66 @@
+# author: Mahmud Ahsan
+# github: https://github.com/mahmudahsan
+# blog: http://thinkdiff.net
+# Web: http://pythonbangla.com
+# youtube: https://www.youtube.com/c/banglaprogramming
+# License: MIT License
+# https://github.com/mahmudahsan/thinkdiff/blob/master/LICENSE 
+
+# --------------------------
+#      Execute JavaScript
+# --------------------------
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from PIL import Image
+from io import BytesIO
+import os
+
+
+def main():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--window-size=1280x1024")
+
+    # download Chrome Webdriver  
+    # https://sites.google.com/a/chromium.org/chromedriver/download
+    # put driver executable file in the script directory
+    chrome_driver = "chromedriver.exe"
+
+    driver = webdriver.Chrome(options=chrome_options, executable_path=chrome_driver)
+
+    driver.get("http://localhost:1947/_int_/features.html")
+    assert "gemalto sentinel acc: features".lower() in driver.title.lower()
+
+    # Find element mydata (licenses table)
+    element = driver.find_element_by_id('mydata')
+    # Get screenshot of full page
+    png = driver.get_screenshot_as_png()
+    # Get location and size of selected element
+    location = element.location
+    size = element.size
+    # Create PIL image to handle screenshot
+    im = Image.open(BytesIO(png))
+    # Get screenshot's border points
+    left = location['x']
+    top = location['y']
+    right = location['x'] + size['width']
+    bottom = location['y'] + size['height']
+
+    # Crop screenshot of element using Pillow
+    im = im.crop((left, top, right, bottom))
+    # Save image in folder
+    im.save('table.png')
+
+    """# fill and submit form
+    elem = driver.find_element_by_name("q")
+    elem.clear()
+    elem.send_keys("python")
+    elem.send_keys(Keys.RETURN)"""
+
+    # screenshot capture of full page
+    driver.get_screenshot_as_file("capture.png")
+    driver.close()
+
+if __name__ == '__main__' : main()
